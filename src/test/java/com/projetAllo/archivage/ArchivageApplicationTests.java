88b7ea8a43bfcc78d7e0748fa36664d616abcd55
romaric-pkrc <romaricpetnga2000@gmail.com>
2022-10-03 13:43:56 +0100

    Initial commit

---
+++ src/test/java/com/projetAllo/archivage/ArchivageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetAllo.archivage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
